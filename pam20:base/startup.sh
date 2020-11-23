5cae0e232557f452c531c789b6114869f9b70236
Eduard Canet <ecanet@profeN2I.informatica.escoladeltreball.org>
2020-11-23 10:18:13 +0100

    base

--- pam20:base/startup.sh
+++ pam20:base/startup.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-useradd user01
-useradd user02
-useradd user03
-echo -e "user01\nuser01\n" | passwd --stdin user01
-echo -e "user02\nuser02\n" | passwd --stdin user02
-echo -e "user02\nuser02\n" | passwd --stdin user02
+useradd unix01
+useradd unix02
+useradd unix03
+echo -e "unix01\nunix01\n" | passwd --stdin unix01
+echo -e "unix02\nunix02\n" | passwd --stdin unix02
+echo -e "unix03\nunix02\n" | passwd --stdin unix02
 
 cp /opt/docker/login.defs /etc/login.defs
 /bin/bash
+
