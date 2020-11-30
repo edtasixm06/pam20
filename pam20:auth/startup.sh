8ba96e3e12b13258ff2d2b01dc258e2572917466
Your Name <you@example.com>
2020-11-30 18:24:06 +0100

    auth

--- pam20:auth/startup.sh
+++ pam20:auth/startup.sh
@@ -13,7 +13,7 @@ bash auth.sh
 #cp /opt/docker/ldap.conf /etc/openldap/ldap.con
 #cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
 #cp /opt/docker/nslcd.conf /etc/nslcd.conf
-cp /opt/docker/system-auth /etc/pam.d/system-auth
+cp /opt/docker/system-auth /etc/pam.d/system-auth-ac
 cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
 authconfig --savebackup ldap
 
